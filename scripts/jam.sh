2f5e1eb3668736e7b39e202a17938a250c6f3a22
ghalestrilo <ghalestrilo@gmail.com>
2022-02-13 23:59:21 -0300

    jamname

--- scripts/jam.sh
+++ scripts/jam.sh
@@ -2,11 +2,13 @@
 
 folder="$1"
 
+jamname=$(date "+%y-%m-%d")
+
 # Run a Tidal Jam
-tmux new-session -s jam "cd $JAM_PATH ;  zsh -c \"nvim\""                                \; \
-  new-window sclang                                                                \; \
-  new-window 'cninjam 143.110.158.146:2049 -user anonymous:ghales -jack'           \; \
-  new-window 'ghci -ghci-script $TIDAL_BOOT_PATH'           \; \
+tmux new-session -s jam "cd $JAM_PATH ;  zsh -c \"nvim include/${jamname}.tidal\""  \; \
+  new-window sclang                                                               \; \
+  new-window 'cninjam 143.110.158.146:2049 -user anonymous:ghales -jack'          \; \
+  new-window 'ghci -ghci-script $TIDAL_BOOT_PATH'                                 \; \
   new-window 'cd ~/soundfonts && fluidsynth "$HOME/soundfonts/Chrono Trigger.sf2" --connect-jack-outputs' \; \
   select-window -t 0
 
