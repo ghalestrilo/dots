ee4fbca7f68c5169879dc170e95ddd76ef72d28a
tg-tg <ghalestrilo@gmail.com>
2025-09-22 11:42:37 -0300

    update make.fihs

--- scripts/make.fish
+++ scripts/make.fish
@@ -18,7 +18,11 @@ curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fi
 ln  common/mise/config.toml $HOME/.config/mise/config.toml
 ln  common/starship.toml $HOME/.config/starship.toml
 ln  common/config.fish $HOME/.config/fish/config.fish
+ln  common/zed.settings.json $HOME/.config/zed/settings.json
 ln  common/code/keybindings.json $HOME/Library/Application\ Support/Cursor/User/keybindings.json
 ln  common/code/settings.json $HOME/Library/Application\ Support/Cursor/User/settings.json
 ln  common/ghostty/config $HOME/Library/Application\ Support/com.mitchellh.ghostty/config
+
+
+
 fisher install jorgebucaran/autopair.fish patrickf1/fzf.fish jhillyerd/plugin-git halostatue/fish-elixir
