a09d6d7bdf64d0a71a5fc2bf11e546accb4a3484
Thales Grilo <thales.grilo@stord.com>
2026-06-26 11:18:15 -0300

    config

--- common/config.fish
+++ common/config.fish
@@ -9,29 +9,25 @@ export DISABLE_BUG_COMMAND=1
 export CLAUDE_CODE_ENABLE_TELEMETRY=0
 export CLAUDE_CODE_DISABLE_NONESSENTIAL_TRAFFIC=true
 
+# Stord
+set -x OBAN_KEY_FINGERPRINT SHA256:4/OSKi0NRF91QVVXlGAhb/BIMLnK8NHcx/EWs+aIWPc
+set -x OBAN_LICENSE_KEY xjhd4hhgkdv2zsu3vv3x53lcwumtvhar
+set -x EDITOR hx
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    # source /opt/homebrew/opt/asdf/libexec/asdf.fish
     fish_add_path "$HOME/.cargo/bin"
     fish_add_path "$HOME/.local/bin"
-    # fish_add_path /opt/homebrew/Caskroom/google-cloud-sdk//latest/google-cloud-sdk/bin/
     export USE_GKE_GCLOUD_AUTH_PLUGIN=True
-    export GOOGLE_APPLICATION_CREDENTIALS=$HOME/.config/gcloud/application_default_credentials.json
 
     source ~/git/dots/common/fly.fish
-
-    if [ -d ~/git/tg ]
-        echo "TG directory found. Importing tg-aliases.fish"
-        source ~/git/dots/scripts/tg-aliases.fish
-    end
-
 
     # Common commands
     alias vault="cd /Users/ghales/Library/CloudStorage/GoogleDrive-ghalestrilo@gmail.com/My\ Drive/me/docs/main"
     alias reload="source ~/.config/fish/config.fish"
     alias theme="kitty +kitten themes"
     alias da="direnv allow ."
-
+    alias version="mise current"
 
     # Elixir commands
     alias mf="mix test --failed --trace"
@@ -49,12 +45,17 @@ if status is-interactive
 
     alias ghostty-config="$EDITOR $HOME/Library/Application\ Support/com.mitchellh.ghostty/config"
 
-    # Set up Homebrew
-    # eval (/opt/homebrew/bin/brew shellenv)
+    # Stord
+    alias sc=stordcli
 
-    # Additional scripts
-    fish_add_path "$HOME/.gem/bin"
-    fish_add_path "$HOME/go/bin"
+    # if [ -d ~/git/stordco ]
+    #     echo "Stord directory found. Importing stord-aliases.fish"
+    #     source ~/git/dots/scripts/stord-aliases.fish
+    # end
+
+    # Set up Homebrew
+    eval (/opt/homebrew/bin/brew shellenv)
+    fish_add_path --move --prepend /opt/homebrew/bin
 
     # Import common functions
     source ~/git/dots/common/functions.fish
